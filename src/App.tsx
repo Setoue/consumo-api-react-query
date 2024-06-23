bec61b688fbc01838c4507cafde1c15d1a83a69e
Setoue <proshigeru@gmail.com>
2024-06-23 01:42:52 -0300

    refactor: use react query to consuming data

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import { useFetch } from "./hooks/useFetch";
+import axios from "axios";
+import { useQuery } from "react-query";
 import { Repository } from "./types/Repository";
 
 function App() {
-  const { data: repositories, isFetching } =
-    useFetch<Repository[]>("users/setoue/repos");
+  const { data, isFetching } = useQuery<Repository[]>("repos", async () => {
+    const response = await axios.get(
+      "https://api.github.com/users/setoue/repos"
+    );
+    return response.data;
+  });
 
   return (
     <ul>
       {isFetching && <p>Carregando...</p>}
-      {repositories?.map((repository) => (
+      {data?.map((repository) => (
         <li key={repository.full_name}>
           <strong>{repository.full_name}</strong>
           <p>{repository.description}</p>
--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
+import { QueryClientProvider } from "react-query";
+import { queryClient } from "./services/queryClient.ts";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
